4e8e991099154d35487262a5331406f28eb00f49
Guilherme Facco Silva <165974357+F4CC0@users.noreply.github.com>
2024-04-08 14:57:01 -0400

    Atualizar o Ex1.py

--- ExercíciosDeFixação/Ex1.py
+++ ExercíciosDeFixação/Ex1.py
@@ -7,3 +7,36 @@ print(q, end="#")
 print(r)
 
 #o sep ou end deveriam separar por # as variaveis
+
+
+
+
+A lógica é essa mesma.
+
+Se a > b:
+
+aux = a (aux salva o valor de a que é o maior pra não perder o valor quando fizer a nova atribuição)
+
+a = b (troca do valor de a, que era maior, por b, que é menor que ele)
+
+b = aux (b recebe o maior valor salvo na variável auxiliar)
+
+se a > c :
+
+aux = a (aux salva o valor de a que é o maior pra não perder o valor quando fizer a nova atribuição)
+
+a = c (troca do valor de a, que era maior, por c, que é menor que ele)
+
+c = aux (b recebe o maior valor salvo na variável auxiliar)
+
+Por fim, precisa comparar B e C, porque já foi verificado a>b e a>c.
+
+Se b > c:
+
+aux = b (aux salva o valor de b que é o maior pra não perder o valor quando fizer a nova atribuição)
+
+b = c (troca do valor de b, que era maior, por c, que é menor que ele)
+
+c = aux (c recebe o maior valor salvo na variável auxiliar)
+
+Espero que ficou entendível
